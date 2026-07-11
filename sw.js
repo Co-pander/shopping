aca9718ad77f3e16bb9c885c46ccb9c09d727a2b
Co-pander <cachettimbre@gmail.com>
2026-07-11 23:53:29 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'shopping-v2';
+const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+// ネットワーク優先・失敗時キャッシュ（更新が反映されやすい方式）
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
